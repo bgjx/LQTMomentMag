e083f7130679621ed772be7697cc9109f1325b83
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-03-07 23:33:04 +0700

    setting setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name= "LQTMomentMag",
+    author= "Arham Zakki Edelo",
+    author_email= "edelo.arham@gmail.com",
+    description= "Calculate seismic moment magnitude in full LQT energy for very local earthquake case",
+    url = ""
+
+
+
+
+)
